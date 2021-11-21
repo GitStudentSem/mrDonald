53638860ef20fbbef1f1a1e36a21a6816ff02334
GitStudentSem <programmSem@yandex.ru>
2021-11-21 18:34:18 +0300

    create project on GIT. lesson 6

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavBar } from "./Components/NavBar";
+import { Menu } from "./Components/Menu";
+import { GlobalStyle } from "./Components/GlobalStyle";
+import { ModalItem } from "./Components/ModalItem";
+import { Order } from "./Components/Order";
+function App() {
+  const [openItem, setOpenItem] = React.useState(null);
+  return (
+    <>
+      <GlobalStyle />
+      <NavBar />
+      <Order />
+      <Menu setOpenItem={setOpenItem} />
+      <ModalItem openItem={openItem} setOpenItem={setOpenItem} />
+    </>
+  );
+}
+
+export default App;
